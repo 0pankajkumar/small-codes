3481ba6dec2b3d77bde5672f0be65acd6ac19663
pankaj.kum <pankaj.kum>
2019-06-21 10:27:36 +0530

    added bfs dfs

---
+++ breadthFirstSearch.py
@@ -0,0 +1,33 @@
+#Breadth first search
+# My first attempt
+
+myGraph = {}
+myGraph['a'] = ['b', 'c']
+myGraph['b'] = ['d', 'e']
+myGraph['c'] = ['f', 'g']
+myGraph['d'] = []
+myGraph['e'] = ['h']
+myGraph['f'] = []
+myGraph['g'] = []
+myGraph['h'] = []
+
+discoveredList = [0] * len(myGraph)
+print(discoveredList)
+def bfs(myGraph,startVertex):
+	Q = []
+	discoveredList[ord(startVertex) - 97] = 1
+	Q.append(startVertex)
+	while Q:
+		#print("Q")
+		v = Q.pop(0)
+		print(v, end=" ")
+		for i in myGraph[v]:
+			#print(i)
+			if discoveredList[ord(i) -97] == 0:
+				Q.append(i)
+				discoveredList[ord(i) - 97] = 1
+				
+		print(discoveredList)
+
+
+bfs(myGraph,'a')
--- competitiveProgramming/codechef/guessway.py
+++ competitiveProgramming/codechef/guessway.py
@@ -1,8 +1,49 @@
-
+# https://www.codechef.com/DI15R080/problems/GUESSWAY
+from pptree import *
 
 class myTreeNode():
-	def __init__(self,val):
+	def __init__(self,test):
 		self.left = None
 		self.right = None
 		self.visited = False
-		self.isExit = val
+		self.isExit = False
+		self.test = test
+		self.isLeaf = False
+
+
+def perfectBinaryTree(root, height):
+	global leafCount
+	height += 1
+	if(height < limit):
+		number = 5
+		root.left = myTreeNode(number)
+		root.right = myTreeNode(number)
+		number = 5
+		if leafCount == 1:
+			root.isExit = True
+		perfectBinaryTree(root.left, height)
+		perfectBinaryTree(root.right, height)
+	else:
+		leafCount -= 1
+		root.isLeaf = True
+
+def Inorder(root):
+	if root:
+		Inorder(root.left)
+		print(f"{root.test}-{root.isLeaf}", end=" ")
+		Inorder(root.right)
+
+
+limit = 4
+number = 0
+leafCount = 3
+
+root = myTreeNode(0)
+perfectBinaryTree(root, 0)
+Inorder(root)
+
+
+# cases = int(input())
+
+# for cas in range(cases):
+# 	h,n = map(int, input().split())
