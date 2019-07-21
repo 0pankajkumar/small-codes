c3c2cd92be26baf0293632ec608fbeaa97a2da1c
Pankaj Kumar <805.bluebell@gmail.com>
2019-07-21 10:57:47 +0530

    started with dijikstra

---
+++ Dijikstra.py
@@ -0,0 +1,13 @@
+# Dijikstra's algorithm
+# My first attempt
+# Psueudo code learnt from https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+
+myGraph = {}
+myGraph[1] = [2,3,6]
+myGraph[2] = ['d', 'e']
+myGraph[3] = ['f', 'g']
+myGraph[4] = []
+myGraph[5] = ['h']
+myGraph[6] = []
+
+
